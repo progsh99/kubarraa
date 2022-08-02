bf60ceb7c34923a7dad45689c2f9ae655bb7b8dc
Programmer Shahad <52330802+progsh99@users.noreply.github.com>
2022-08-02 18:51:43 +0300

    Add files via upload
    first

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->String('address');
+            $table->String('phone1')->nullable();
+            $table->String('phone2')->nullable();
+            $table->String('phone3')->nullable();
+            $table->String('mainid')->unique();
+            $table->String('city');
+            $table->String('phone');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
